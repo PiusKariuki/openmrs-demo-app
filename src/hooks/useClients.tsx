59bb9dd13e2f9ab356d1e09f88862a879d81f668
pius <piuskariuki19@gmail.com>
2024-09-05 21:36:48 +0300

    chore: table working

---
+++ src/hooks/useClients.tsx
@@ -0,0 +1,60 @@
+import useSWR from "swr";
+import { fhirBaseUrl, openmrsFetch } from "@openmrs/esm-framework";
+import { useEffect, useState } from 'react';
+import { Link } from '@carbon/react';
+import React from 'react';
+
+export interface PatientObject{
+  id: string;
+  name: string;
+  birthDate: string;
+  gender: string
+}
+
+
+export interface TableHeadConfig {
+  key: string;
+  header: string;
+}
+
+export const useClients = () => {
+  const [cleanData, setCleanData] = useState<Array<PatientObject>>([])
+
+  const { data, error, isLoading } = useSWR<any, Error>(
+    `${fhirBaseUrl}/Patient`,
+    openmrsFetch
+  );
+
+  useEffect(()=>{
+    const baseUrl = window.getOpenmrsSpaBase()
+
+    if(data?.data?.entry.length>0){
+      const formattedArray = data.data.entry.map(entry => ({
+        id: entry.resource.id,
+        name: <Link href={`${baseUrl}patient/${entry.resource.id}/chart`}>{entry.resource.name[0].text}</Link>,
+        birthDate: entry.resource.birthDate,
+        gender: entry.resource.gender,
+      }));
+
+      setCleanData(formattedArray);
+    }
+  }, [data])
+
+  const tableHeaders = [
+    {
+      key: "name",
+      header: "Full name"
+    },
+    {
+      key: "birthDate",
+      header: "Date of Birth",
+    },
+    {
+      key: "gender",
+      header: "Gender",
+    }
+  ]
+
+
+  return  {data, error, isLoading, tableHeaders, cleanData};
+};
--- src/views/home.component.tsx
+++ src/views/home.component.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import { useClients, PatientObject, TableHeadConfig } from '../hooks/useClients';
+import styles from "./home.component.scss"
+import {
+  DataTable,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@carbon/react';
+
 
 
 const Home: React.FC<{}> = () => {
+
+  const {tableHeaders, cleanData} = useClients()
+
   return(
-    <div>Hello</div>
+    <div className={styles.page}>
+      <DataTable rows={cleanData} headers={tableHeaders}
+      render={({rows, headers, getTableProps, getHeaderProps, getRowProps})=>(
+        <TableContainer title="List of clients">
+          <Table {...getTableProps}>
+            <TableHead>
+              <TableRow>
+                {headers.map((header: TableHeadConfig) => (
+                  <TableHeader {...getHeaderProps({header})}>{header.header}</TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                rows.map((row) => (
+                  <TableRow {...getRowProps({row})}>
+                    {row.cells.map(cell =>(
+                      <TableCell key={cell.id} >{cell.value}</TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+
+      >
+        <TableHead></TableHead>
+      </DataTable>
+    </div>
   )
 }
 
