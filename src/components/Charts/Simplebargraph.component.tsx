6aee3c79f95df38f3b61212c93c7931a0e0d18d7
Samuel Muli <samuelmuli95@gmail.com>
2024-09-09 11:06:43 +0300

    Create simple bar chart

---
+++ src/components/Charts/Simplebargraph.component.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { SimpleBarChart } from '@carbon/charts-react';
+import '@carbon/charts-react/styles.css';
+import { ScaleTypes } from '@carbon/charts-react';
+
+export default function Simplebargraph() {
+	const options = {
+		axes: {
+			left: {
+				mapsTo: 'y',
+				title: 'Count',
+			},
+			bottom: {
+				mapsTo: 'x',
+				scaleType: ScaleTypes.LABELS,
+				title: 'Days',
+			}
+		}
+	}
+
+	const data = [
+		{
+			x: 'Mon',
+			y: 10
+		},
+		{
+			x: 'Tue',
+			y: 15
+		},
+		{
+			x: 'Wed',
+			y: 25
+		},
+		{
+			x: 'Thu',
+			y: 30
+		}
+	]
+
+    return (
+        <div style={{ height: '300px' }}>
+			<SimpleBarChart
+				data={data}
+				options={options}
+			></SimpleBarChart>
+		</div>
+    )
+}
--- src/views/home.component.tsx
+++ src/views/home.component.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './home.component.scss';
 import { Extension, ExtensionSlot } from '@openmrs/esm-framework';
+import Simplebargraph from '../components/Charts/Simplebargraph.component';
 
 const Home: React.FC<{}> = () => {
   return (
     <div className={styles.page}>
       <ExtensionSlot name="clients">
-        <Extension />
+        {/* <Extension /> */}
+        <Simplebargraph />
       </ExtensionSlot>
     </div>
   );
